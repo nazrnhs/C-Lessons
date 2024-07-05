f20dcfe9d1078a1216e47764f3d7e23e9bbd5c82
Nazrin <141553840+nazrnhs@users.noreply.github.com>
2024-07-05 11:55:58 +0400

    ASP.Net.MVC

---
+++ BizlandWeb/BizlandWeb/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action}/{id?}"
+    );
+
+app.Run();
